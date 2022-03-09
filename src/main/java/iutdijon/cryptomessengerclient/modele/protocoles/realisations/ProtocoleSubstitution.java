68c51e40c64672e96c686ebc7b91df5aa1f1719b
Alexis-Nicolas <alexis.nicolas@iut-dijon.u-bourgogne.fr>
2022-03-09 08:38:11 +0100

    yes

--- src/main/java/iutdijon/cryptomessengerclient/modele/protocoles/realisations/ProtocoleSubstitution.java
+++ src/main/java/iutdijon/cryptomessengerclient/modele/protocoles/realisations/ProtocoleSubstitution.java
@@ -5,10 +5,23 @@
  */
 package iutdijon.cryptomessengerclient.modele.protocoles.realisations;
 
+import iutdijon.cryptomessengerclient.modele.messages.Message;
+import iutdijon.cryptomessengerclient.modele.protocoles.Protocole;
+
 /**
  *
  * @author alexi
  */
-public class ProtocoleSubstitution {
+public class ProtocoleSubstitution extends Protocole{
+
+    @Override
+    public Message chiffrer(Message messageClair) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Message dechiffrer(Message messageChiffre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
