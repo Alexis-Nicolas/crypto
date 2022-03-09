a236aca6c9d82af2726688a9d42b53aa395c2d94
Alexis-Nicolas <alexis.nicolas@iut-dijon.u-bourgogne.fr>
2022-03-09 11:32:53 +0100

    chiffersub

--- src/main/java/iutdijon/cryptomessengerclient/modele/protocoles/realisations/ProtocoleSubstitution.java
+++ src/main/java/iutdijon/cryptomessengerclient/modele/protocoles/realisations/ProtocoleSubstitution.java
@@ -16,7 +16,48 @@ public class ProtocoleSubstitution extends Protocole{
 
     @Override
     public Message chiffrer(Message messageClair) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char A = 'A';
+        char Z = 'Z';
+        char a = 'a';
+        char z = 'z';
+        String cle = this.getCle("CLE_SYMETRIQUE");
+        char stockage ;
+        String resultat ="" ;
+        Message reponse= messageClair;
+        String message = reponse.getCorpsMessage();
+        StringBuilder en = new StringBuilder();   
+        boolean v = false;
+        for(char c:message.toCharArray()){
+            if(Character.isLetter(c)){
+                int i =Character.compare(c,A);
+                if((i>=0)&&(Character.compare(c,Z)<=0)){
+                    v = true;
+                    for(int y=0;y<=alphabet.length();y++){
+                       if (c==alphabet.charAt(y)) {
+                           int position = y;
+                           resultat += cle.charAt(y);
+                       }
+                    }
+                }else if((Character.compare(c,z)<=0)&&(Character.compare(c,a)>=0)){
+                    v=false;
+                    for(int y=0;y<=alphabet.length();y++){
+                        c=Character.toUpperCase(c);
+                        if (c==alphabet.charAt(y)) {
+                           int position = y;
+                           stockage = cle.charAt(y);
+                           
+                           resultat += Character.toLowerCase(stockage);
+                       }
+                    }
+                }
+                    
+                }else{
+                resultat+=c;
+            }
+        }
+        reponse.setCorpsMessage(en.toString());
+        return reponse;
     }
 
     @Override
